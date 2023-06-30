f59569675696be7dffc9bc6c2afeaa060fb7bf65
Chương Đoàn <chuongdoan2102@gmail.com>
2023-06-30 21:18:26 +0700

    F.R.I.E.N.D.S

--- Loka/Program.cs
+++ Loka/Program.cs
@@ -7,6 +7,16 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.RegisterServices();
 
+// Fix CORS
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+                      builder =>
+                      {
+                          builder.WithOrigins("https://localhost:3000");
+                      });
+});
+
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataLokaContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionStringName")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
