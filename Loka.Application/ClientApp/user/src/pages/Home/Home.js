e77ff1a67340062bf75849a73447bc33f76b8394
Đào Duy An <daoduyan8801@gmail.com>
2023-07-10 16:20:33 +0700

    responsive

--- Loka.Application/ClientApp/user/src/pages/Home/Home.js
+++ Loka.Application/ClientApp/user/src/pages/Home/Home.js
@@ -43,8 +43,8 @@ function Home() {
     // }, []);
 
     return (
-        <div className={cx('wrapper', 'my-[100px]')}>
-            <div className="grid grid-cols-4 gap-x-14 gap-y-16">
+        <div className={cx('wrapper')}>
+            <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-x-14 gap-y-16">
                 <Link to={config.routes.detail}>
                     <div
                         className={cx('post__item', 'w-auto', 'flex', 'flex-col', 'justify-between', 'rounded-xl', {
--- Loka.Application/ClientApp/user/src/pages/SearchResult/SearchResult.js
+++ Loka.Application/ClientApp/user/src/pages/SearchResult/SearchResult.js
@@ -39,7 +39,7 @@ function SearchResult() {
     return (
             <div className={cx('wrapper', 'my-[30px]')}>
                 <h2 className={cx('mb-[30px]', 'text-[20px]', 'font-medium')}>Kết quả tìm kiếm: {keyword}</h2>
-                <div className="grid grid-cols-4 gap-x-14 gap-y-16">
+                <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-x-14 gap-y-16">
                 <Link to={config.routes.detail}>
                     <div
                         className={cx('post__item', 'w-auto', 'flex', 'flex-col', 'justify-between', 'rounded-xl', {
